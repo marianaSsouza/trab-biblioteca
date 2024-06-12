942484b606a7687fc617453de9c869fab2f2cd84
Mariana <marianabatistella3@gmail.com>
2024-06-11 21:02:27 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+      
+
+        InteracaoComUsuario interacao = new InteracaoComUsuario();
+
+        int opcao = 0;
+
+        
+
+
+    }
+}
---
+++ src/interacaoComUsuario.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class interacaoComUsuario {
+
+    private final Scanner leitor;
+
+    public int mostrarOpcaoMenu(Scanner leitor ){
+        this.leitor = leitor; 
+    } 
+
+    final String Menu = """
+      ----menu----
+    1. bla
+    2. blbla
+    3.blablabla
+
+            
+            """;
+       
+ 
+        return leitor.nextInt();
+
+        public int interacao(
+            
+        )
+
+
+    }
+   
+
+}

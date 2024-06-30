ad00b8f7dd9edd223abf5b1415d77a4d5f13dd20
Mariana de Souza <168456934+marianaSsouza@users.noreply.github.com>
2024-06-30 16:22:45 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -16,12 +16,41 @@ public class App {
 
         /// lista com todos os livros
         List<Livro> livros = new ArrayList<Livro>();
-        livros.add(new Livro("livro1", "ficção", 30.0));
-        livros.add(new Livro("livro3", "ação", 20.0));
-        livros.add(new Livro("livro4", "ciência", 20.0));
-        livros.add(new Livro("livro4", "filosofia", 20.0));
-        livros.add(new Livro("livro5", "terror", 20.0));
-        livros.add(new Livro("livro2", "suspense", 10.0));
+
+        //ficção
+        livros.add(new Livro("harry potter", "ficção", 60.0));;
+        livros.add(new Livro("o pequeno principe", "ficção", 20.0));;
+        livros.add(new Livro("q revolução dos bichos", "ficção", 15.0));;
+        livros.add(new Livro("o paraíso", "ficção", 20.0));;
+        livros.add(new Livro("o hobbit", "ficção", 56.0));;
+
+        //ciencia 
+        livros.add(new Livro("cosmos", "ciência", 56.0));
+        livros.add(new Livro("do atomo ao buraco negro", "ciência", 55.0));
+        livros.add(new Livro("o despertar da consciencia", "ciência", 150.0));
+        livros.add(new Livro("teoria quantica", "ciência", 27.0));
+        livros.add(new Livro("a clavicula de salomão", "ciência", 68.0));
+
+        //filosofia
+        livros.add(new Livro("além do bem e do mal", "filosofia", 28.0));
+        livros.add(new Livro("assim falou zaratustra", "filosofia", 35.0));
+        livros.add(new Livro("genealogia da moral", "filosofia", 35.0));
+        livros.add(new Livro("o idiota", "filosofia", 105.0));
+        livros.add(new Livro("o catador de pensamentos", "filosofia", 40.0));
+
+        //terror
+        livros.add(new Livro("frankenstein", "terror", 50.0));
+        livros.add(new Livro("a hora do vampiro", "terror", 46.0));
+        livros.add(new Livro("necronomicon", "terror", 76.0));
+        livros.add(new Livro("entre sonhos e pesadelos ", "terror", 44.90));
+        livros.add(new Livro("a mortalha carmesin", "terror", 49.90));
+
+        //supense 
+        livros.add(new Livro("manual de assassinato", "suspense", 47.0));
+        livros.add(new Livro("a cirurgiã", "suspense", 46.0));
+        livros.add(new Livro("o fabricante de lagrimas", "suspense", 55.0));
+        livros.add(new Livro("outsider", "suspense", 75.0));
+        livros.add(new Livro("ninguem vai te ouvir gritar", "suspense", 40.0));
 
         // lista para o carrinho de compras
         List<Livro> carrinho = new ArrayList<>();
@@ -81,7 +110,7 @@ public class App {
 
         leitor.close();
 
-       
+     
 
     }
 }
